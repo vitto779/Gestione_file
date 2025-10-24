fdc320eb20f8cdfdffa049de0cae1b1528aa0c31
Vittorio Gabbrielli <VITTORIO.GABBRIELLI@local.ittcesena.it>
2025-10-24 09:23:17 +0200

    R0.1 :Completata la classe writer

--- gestione_file/Writer.cs
+++ gestione_file/Writer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,33 @@ namespace gestione_file
 {
     internal class Writer
     {
+        private string filePath;
+
+        public Writer(string path) 
+        {
+            filePath = path; 
+        }
+
+        public void ScriviFile() 
+        {
+            try
+            {
+                using(StreamWriter writer = new StreamWriter(filePath))
+                {
+                    writer.WriteLine("Prima riga");
+                    writer.WriteLine("Seconda riga");
+                    writer.WriteLine("Terza linea");
+
+                }
+
+                Console.WriteLine("File scritto con succeso");
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("errore nella scritttura del file" + ex.Message);
+            }
+        }
+
     }
 }
