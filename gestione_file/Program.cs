e56297c7051d6fbd965ca5e2da49a89a5f1e62c0
vitto779 <vittoriogabbrielli779@gmail.com>
2025-10-31 08:27:12 +0100

    Update Program.cs
    commento provas

--- gestione_file/Program.cs
+++ gestione_file/Program.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+//commento di prova
 namespace gestione_file
 {
     internal class Program
